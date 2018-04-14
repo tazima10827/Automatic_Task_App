8b625c7cd8386a373449c3431824b77a65dbfc8b
Motohiro Tajima <kapiha7@gmail.com>
2018-04-14 17:37:24 +0900

    Initial Commit

---
+++ Automatic_Task_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Automatic_Task_App
+//
+//  Created by 田嶋智洋 on 2018/04/14.
+//  Copyright © 2018年 田嶋智洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
